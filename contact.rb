93601fc541aa97772dc11cd1241edb5336b8ea41
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-16 15:53:49 -0400

    finished exercise

--- contact.rb
+++ contact.rb
@@ -1,8 +1,9 @@
 class Contact
   attr_accessor :first_name, :last_name, :email, :notes
+  attr_reader :id
 
-
-  def initialize(first_name, last_name, email, notes)
+  def initialize(id, first_name, last_name, email, notes)
+    @id = id
     @first_name = first_name.capitalize
     @last_name = last_name.capitalize
     @email = email
@@ -10,8 +11,10 @@ class Contact
   end
 
   def full_name
-  "#{first_name} #{last_name }"
+    "#{first_name} #{last_name}"
   end
 
-
+  def to_s
+    "#{id}: #{full_name} <#{email}> -- Notes: #{notes}"
+  end
 end
