ffe9f8d5cff2670036f0c4768c9867dd45190ae2
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-16 15:28:22 -0400

    aded beginning of form to input new contacts

--- crm.rb
+++ crm.rb
@@ -4,7 +4,7 @@ require_relative 'contact'
 require 'sinatra'
 require 'sinatra/reloader'
 
-
+$rolodex= Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -13,10 +13,5 @@ end
 
 
 get '/contacts' do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-founder")
-
   erb :contacts
 end
