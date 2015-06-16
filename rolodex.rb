7dbcece602ed27b28713a5a6a6e53e8fce0a90e6
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-16 16:06:56 -0400

    fixed bugs in last post- actual finished exercise

--- rolodex.rb
+++ rolodex.rb
@@ -4,16 +4,11 @@ class Rolodex
   def initialize
     @contacts = []
     @id = 1000
-
-    add_test_data!
   end
 
-  def add_test_data!
-    add_contact("Fakey", "McFAkerson", "real@email.email", "Super real")
-  end
 
-  def add_contact(first_name, last_name, email, notes)
-    contact = Contact.new(@id, first_name, last_name, email, notes)
+  def add_contact(contact)
+    contact.id = @id
     @contacts << contact
     @id += 1
   end
