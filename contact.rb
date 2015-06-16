ccb1ead53e952587f24244a90cf6071f89695c8d
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-16 15:12:57 -0400

    updated contact file - now a working contact.erb and contact.rb

--- contact.rb
+++ contact.rb
@@ -1,9 +1,8 @@
 class Contact
   attr_accessor :first_name, :last_name, :email, :notes
-  attr_reader :id
 
-  def initialize(id, first_name, last_name, email, notes)
-    @id = id
+
+  def initialize(first_name, last_name, email, notes)
     @first_name = first_name.capitalize
     @last_name = last_name.capitalize
     @email = email
@@ -13,7 +12,6 @@ class Contact
   def full_name
   "#{first_name} #{last_name }"
   end
-
 
 
 end
--- crm.rb
+++ crm.rb
@@ -1,5 +1,8 @@
+require_relative 'contact'
+
 require 'sinatra'
 require 'sinatra/reloader'
+
 
 
 get '/' do
@@ -9,6 +12,10 @@ end
 
 
 get '/contacts' do
-  @contacts = "contacts"
+  @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-founder")
+
   erb :contacts
 end
