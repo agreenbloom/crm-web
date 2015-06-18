872c4559577b9765ec28c4e708fa209e44724718
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-18 13:35:33 -0400

    added method to find contact

--- crm.rb
+++ crm.rb
@@ -5,6 +5,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("John", "Doe", "johnD@gmail.com", "Awesome"))
+
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -19,9 +21,14 @@ get '/contacts/new' do
   erb :new_contact
 end
 
+get '/contact/1' do
+  @contact = @@rolodex.find(1)
+  erb :show_contact
+end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
--- rolodex.rb
+++ rolodex.rb
@@ -3,8 +3,9 @@ class Rolodex
 
   def initialize
     @contacts = []
-    @id = 1000
+    @id = 1
   end
+
 
 
   def add_contact(contact)
@@ -12,4 +13,9 @@ class Rolodex
     @contacts << contact
     @id += 1
   end
+
+  def find(contact_id)
+    @contacts.find {|contact| contact.id == contact_id }
+  end
+
 end
