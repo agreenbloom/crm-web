950e454252cd2d7bccb876a850a73d21d5e77097
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-18 15:30:52 -0400

    fixed bugs in crm

--- contact.rb
+++ contact.rb
@@ -12,4 +12,10 @@ class Contact
     "#{first_name} #{last_name}"
   end
 
+  def update(first_name, last_name, email, notes)
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
+    @email = email
+    @notes = notes
+  end
 end
--- crm.rb
+++ crm.rb
@@ -4,10 +4,8 @@ require_relative 'contact'
 require 'sinatra'
 require 'sinatra/reloader'
 
-$rolodex = Rolodex.new
-$rolodex.add_contact(Contact.new("John", "Doe", "johnD@gmail.com", "Awesome"))
-$rolodex.add_contact(Contact.new("Joe", "Shmo", "js@hello.com", "Hello"))
-$rolodex.add_contact(Contact.new("Peter", "Griffin", "familyguy@cartoon.com", "whatsup"))
+@@rolodex = Rolodex.new
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -22,16 +20,10 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-get '/contact/:id' do
+get '/contacts/:id' do
   @contact = @@rolodex.find(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email  = params[:email]
-    @contact.note = params[:note]
-
-    redirect to("/contacts")
-
+    erb :show_contact
   else
     raise Sinatra::NotFound
   end
@@ -42,11 +34,11 @@ get '/contacts/:id/edit' do
   if @contact
     erb :edit_contact
   else
-    Sinatra::NotFound
+    raise Sinatra::NotFound
   end
 end
 
-delete "/contacs/:id" do
+delete "/contacts/:id" do
   @contact = @@rolodex.find(params[:id].to_i)
   if @contact
     @@rolodex.remove_contact(@contact)
@@ -56,9 +48,20 @@ delete "/contacs/:id" do
   end
 end
 
-post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
-  redirect to('/contacts')
+post "/contacts" do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
+  @@rolodex.add_contact(new_contact)
+  redirect to("/contacts")
 end
+
+put '/contacts/:id' do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.update(params[:first_name], params[:last_name], params[:email], params[:notes])
+    redirect to("/contacts/#{@contact.id}")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 
